80c67fe31150640c20bbe2ec699e9c63a96e8fe5
junhui1997 <57162618+junhui1997@users.noreply.github.com>
2023-03-12 12:40:59 -0400

    add jigsaw version

--- mrg/surgical_gesture_recognition_jigsaw/scripts/mrg.sh
+++ mrg/surgical_gesture_recognition_jigsaw/scripts/mrg.sh
@@ -1,5 +1,9 @@
 cd ..
-python -u tcn_main.py --split 1 --save_name hhh
-python -u tcn_main.py --split 2 --save_name hhh
-python -u tcn_main.py --split 3 --save_name hhh
+python -u tcn_main.py --split 1 --save_name hhh --task_name kt --seq_limit 300 --enc_in 76
+python -u tcn_main.py --split 2 --save_name hhh --task_name kt --seq_limit 300 --enc_in 76
+python -u tcn_main.py --split 3 --save_name hhh --task_name kt --seq_limit 300 --enc_in 76
+#
+python -u tcn_main.py --split 1 --save_name hhh --task_name kt --seq_limit 600 --enc_in 76
+python -u tcn_main.py --split 2 --save_name hhh --task_name kt --seq_limit 600 --enc_in 76
+python -u tcn_main.py --split 3 --save_name hhh --task_name kt --seq_limit 600 --enc_in 76
 ###
