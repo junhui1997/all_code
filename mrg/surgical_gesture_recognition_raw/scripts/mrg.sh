91ff09469dbbe443947d31b04dad60d31ec4167d
junhui1997 <57162618+junhui1997@users.noreply.github.com>
2023-03-15 05:31:17 -0400

    add raw feature

--- mrg/surgical_gesture_recognition_raw/scripts/mrg.sh
+++ mrg/surgical_gesture_recognition_raw/scripts/mrg.sh
@@ -1,5 +1,10 @@
 cd ..
-python -u tcn_main.py --split 1 --save_name hhh
-python -u tcn_main.py --split 2 --save_name hhh
-python -u tcn_main.py --split 3 --save_name hhh
+python -u tcn_main.py --split 1 --save_name hhh --task_name su
+python -u tcn_main.py --split 2 --save_name hhh --task_name su
+python -u tcn_main.py --split 3 --save_name hhh --task_name su
+python -u tcn_main.py --split 4 --save_name hhh --task_name su
+python -u tcn_main.py --split 5 --save_name hhh --task_name su
+python -u tcn_main.py --split 6 --save_name hhh --task_name su
+python -u tcn_main.py --split 7 --save_name hhh --task_name su
+python -u tcn_main.py --split 8 --save_name hhh --task_name su
 ###
