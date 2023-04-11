2fb3b55fcdef5dfc02380ebe6be7eb4e59a3e691
u <jh4165@columbia.edu>
2023-04-11 16:28:41 +0800

    from u

--- Time-Series-Library/data_provider/data_loader_bone_drill.py
+++ Time-Series-Library/data_provider/data_loader_bone_drill.py
@@ -75,7 +75,7 @@ class Dataset_bone_drill(Dataset):
         '''
         df_raw.columns: ['date', ...(other features), target feature]
         '''
-        df_raw = df_raw[::50]
+        df_raw = df_raw[::2]
         # 添加是否使用滤波器
         if self.args.filter != 'no_filter':
             df_raw = apply_filter(df_raw, self.args)
