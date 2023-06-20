5413826886fae00dd03e627bafa549eefc1e85b2
junhui1997 <57162618+junhui1997@users.noreply.github.com>
2023-06-20 12:46:01 +0800

    change fcn_m

--- Time-Series-Library/scripts/bone_drill/test_myc.sh
+++ Time-Series-Library/scripts/bone_drill/test_myc.sh
@@ -10,7 +10,7 @@ do
   for factor in 1 2 3
 do
   python -u run.py \
-  --task_name classification --enc_in 12   --is_training 1    --root_path ./    --model_id bone_c   --data bone_drill_c    --e_layers $e_layers  --d_layers $d_layers    --batch_size 16    --d_model 64  --factor $factor  --d_ff 64    --top_k 3    --des 'Exp'    --itr 50    --learning_rate 0.0001    --train_epochs 40    --patience 15 --seq_len $seq_len --filter mean --model $model_name
+  --task_name classification --enc_in 12   --is_training 1    --root_path ./    --model_id bone_c   --data bone_drill_c    --e_layers $e_layers  --d_layers $d_layers    --batch_size 16    --d_model 64  --factor $factor  --d_ff 64    --top_k 3    --des 'Exp'    --itr 25    --learning_rate 0.0001    --train_epochs 40    --patience 15 --seq_len $seq_len --filter mean --model $model_name
 done
 done
 done
